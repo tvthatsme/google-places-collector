c92327eaa7bd2c8b1f30091a68d8400c02d18eaa
Timothy Vernon <timothy.n.vernon@gmail.com>
2018-04-13 19:26:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Google Places Collector
+
+Harness the power of the Google Maps API to collect information for geographical locations within a city.
+
+## Usage
+
+Get an API Key from google gain permissions to places data. You can either define a property in a seperates .env file under GOOGLE_MAPS_KEY or
+replace the 'YOUR_KEY_HERE' string in the index.js file.
---
+++ index.js
@@ -0,0 +1,155 @@
+require('dotenv').config();
+
+const uniqBy = require('lodash/uniqBy');
+const has = require('lodash/has');
+const isPointInPolygon = require('point-in-polygon');
+const googleMaps = require('@google/maps').createClient({
+  key: process.env.GOOGLE_MAPS_KEY || 'YOUR_KEY_HERE'
+});
+
+/**
+ * Determine if a place is located within a geographic location
+ *
+ * @param {Object} place latitude and longitude of place
+ * @param {Object} area southwest and northeast bounds of area
+ */
+const isPlaceInArea = (place, area) => {
+  // Create a marker for the place's location
+  const placePnt = [place.lat, place.lng];
+  // Create a polygon using the area's southwest and northeast bounds
+  const areaPolygon = [
+    [area.southwest.lat, area.southwest.lng],
+    [area.northeast.lat, area.southwest.lng],
+    [area.northeast.lat, area.northeast.lng],
+    [area.southwest.lat, area.northeast.lng]
+  ];
+
+  return isPointInPolygon(placePnt, areaPolygon);
+};
+
+/**
+ * Determine if a place has enough reviews on Google to prove that it
+ * is a legitimate place. Having a rating should help to filter
+ * out the ghost shops or places that haven't opened yet.
+ *
+ * @param {Object} place Place of interest than needs to be validated
+ */
+const hasEnoughReviews = place => {
+  // For now having any kind of rating is enough to show that this might
+  // be a valid place. However, this might need to change in the future
+  // to be a bit more strict.
+  return has(place, 'rating');
+};
+
+/**
+ * Provided a location, get all the places nearby
+ *
+ * @param {*} location
+ * @param {*} radius
+ * @param {*} type
+ */
+const getAllNearbyPlaces = (location, type = '*') => {
+  // TODO: there might be some reasons for making radius more dynamic in the future
+  let params = {
+    language: 'en',
+    location,
+    radius: 1000
+  };
+
+  if (type !== '*') {
+    params['type'] = type;
+  }
+
+  return new Promise((resolve, reject) => {
+    googleMaps.placesNearby(params, (err, response) => {
+      if (!err) {
+        resolve(response.json.results);
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
+
+/**
+ * Get the bounding coordinates of an area
+ *
+ * @param {String} address name of area or place
+ */
+const getAreaCoordinates = address => {
+  return new Promise((resolve, reject) => {
+    googleMaps.geocode(
+      {
+        address,
+        language: 'en'
+      },
+      (err, response) => {
+        if (!err) {
+          const matches = response.json.results;
+          if (matches.length) {
+            resolve(matches[0].geometry.bounds);
+          } else {
+            reject('none found');
+          }
+        } else {
+          reject(err);
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Get an array of places matching a certain type within an area
+ *
+ * @param {String} locationName name of the area to search in
+ * @param {String} locationPoints type of place to search for
+ */
+async function getPlacesByType(locationName, locationPoints) {
+  const areaCoords = await getAreaCoordinates(locationName);
+  const increments = 5;
+  const latIncrement =
+    Math.abs(areaCoords.northeast.lat - areaCoords.southwest.lat) / increments;
+  const lngIncrement =
+    Math.abs(areaCoords.northeast.lng - areaCoords.southwest.lng) / increments;
+
+  let pointsInArea = [];
+
+  // Create a 'battleship'-like grid covering the area
+  // * * * * * X
+  // * * * * * *
+  // * * * * * *
+  // * * * * * *
+  // * * * * * *
+  // X * * * * *
+  for (let i = 0; i <= increments; i++) {
+    for (let j = 0; j <= increments; j++) {
+      pointsInArea.push({
+        lat: (areaCoords.southwest.lat + latIncrement * j).toFixed(7),
+        lng: (areaCoords.southwest.lng + lngIncrement * i).toFixed(7)
+      });
+    }
+  }
+
+  const results = await Promise.all(
+    pointsInArea.map(async point => {
+      return await getAllNearbyPlaces(point, locationPoints);
+    })
+  );
+
+  // flatten and filter
+  const places = results
+    .reduce((a, b) => a.concat(b), [])
+    .filter(place => isPlaceInArea(place.geometry.location, areaCoords))
+    .filter(place => hasEnoughReviews(place));
+
+  // get unique
+  const uniquePlaces = uniqBy(places, place => place.id);
+
+  console.log(uniquePlaces);
+  console.log(`${uniquePlaces.length} found`);
+
+  return uniquePlaces;
+}
+
+getPlacesByType('Business Bay', 'restaurant');
